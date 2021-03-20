4d20b98cb108bb99f27f3d8b0c448372050f0ed0
yasir faiz ahmed <46210222+yasirfaizahmed@users.noreply.github.com>
2021-03-20 15:26:00 +0530

    Update GUI2.py

--- GUI_designs/GUI_v0/GUI2.py
+++ GUI_designs/GUI_v0/GUI2.py
@@ -12,7 +12,7 @@ from kivy.core.text import LabelBase
 from kivy import clock
 from kivy.properties import DictProperty
 
-
+from kivy.uix.button import Button 
 
 LabelBase.register(fn_regular='Orbitron-VariableFont_wght.ttf', name='myfont')
 
@@ -23,17 +23,11 @@ Window.fullscreen = True
 
 
 
-class FloatLayout(FloatLayout):    #root widget, main logic class
-
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-
-
-
+### main App
 
 class dashboardApp(App):
+    
+    
     variables = DictProperty({"speed": 25, "kms_ran": 1500, "mileage": 56, "battery_percentage": 49, "btry_crg_sts": False, "battery_temp": 32, "heart_rate": 72, "oxygen": 69})
 
 
@@ -81,6 +75,161 @@ class dashboardApp(App):
         clock.Clock.schedule_interval(self.update, 1)
 
         return FloatLayout()
+
+
+
+
+
+
+###  Credential GUI
+class pinApp(App):
+    pin = "6969"
+    pin_length = 4
+    pin_index = 0
+    input_text = ""
+    
+    
+        
+    
+    
+    def build(self):
+      layout = FloatLayout()
+      
+      b0 = Button(pos_hint={'x': .5, 'center_y': .2}, size_hint=(.2, .2),text= '0')
+      b1 = Button(pos_hint={'x': .3, 'center_y': .8}, size_hint=(.2, .2),text= '1')
+      b2 = Button(pos_hint={'x': .5, 'center_y': .8}, size_hint=(.2, .2),text= '2')
+      b3 = Button(pos_hint={'x': .7, 'center_y': .8}, size_hint=(.2, .2),text= '3')
+      b4 = Button(pos_hint={'x': .3, 'center_y': .6}, size_hint=(.2, .2),text= '4')
+      b5 = Button(pos_hint={'x': .5, 'center_y': .6}, size_hint=(.2, .2),text= '5')
+      b6 = Button(pos_hint={'x': .7, 'center_y': .6}, size_hint=(.2, .2),text= '6')
+      b7 = Button(pos_hint={'x': .3, 'center_y': .4}, size_hint=(.2, .2),text= '7')
+      b8 = Button(pos_hint={'x': .5, 'center_y': .4}, size_hint=(.2, .2),text= '8')
+      b9 = Button(pos_hint={'x': .7, 'center_y': .4}, size_hint=(.2, .2),text= '9')
+      b_clear = Button(pos_hint={'x': .7, 'center_y': .2}, size_hint=(.2, .2),text= 'clear')
+      layout.add_widget(b0)
+      layout.add_widget(b1)
+      layout.add_widget(b2)
+      layout.add_widget(b3)
+      layout.add_widget(b4)
+      layout.add_widget(b5)
+      layout.add_widget(b6)
+      layout.add_widget(b7)
+      layout.add_widget(b8)
+      layout.add_widget(b9)
+      layout.add_widget(b_clear)
+      
+      
+      b0.bind(on_press = self.callback_0)
+      b1.bind(on_press = self.callback_1)
+      b2.bind(on_press = self.callback_2)
+      b3.bind(on_press = self.callback_3)
+      b4.bind(on_press = self.callback_4)
+      b5.bind(on_press = self.callback_5)
+      b6.bind(on_press = self.callback_6)
+      b7.bind(on_press = self.callback_7)
+      b8.bind(on_press = self.callback_8)
+      b9.bind(on_press = self.callback_9)
+      b_clear.bind(on_press = self.callback_clear)
+      
+      return layout
+      
+    def callback_0(self, event): 
+        self.input_text += '0'
+        self.pin_index += 1
+        if self.pin_index == self.pin_length:
+            if self.input_text == self.pin:
+                App.get_running_app().stop()
+                dashboardApp().run()
+    def callback_1(self, event): 
+        self.input_text += '1'
+        self.pin_index += 1
+        if self.pin_index == self.pin_length:
+            if self.input_text == self.pin:
+                App.get_running_app().stop()
+                dashboardApp().run()
+    def callback_2(self, event): 
+        self.input_text += '2'
+        self.pin_index += 1
+        if self.pin_index == self.pin_length:
+            if self.input_text == self.pin:
+                App.get_running_app().stop()
+                dashboardApp().run()
+    def callback_3(self, event): 
+        self.input_text += '3'
+        self.pin_index += 1
+        if self.pin_index == self.pin_length:
+            if self.input_text == self.pin:
+                App.get_running_app().stop()
+                dashboardApp().run()
+    def callback_4(self, event): 
+        self.input_text += '4'
+        self.pin_index += 1
+        if self.pin_index == self.pin_length:
+            if self.input_text == self.pin:
+                App.get_running_app().stop()
+                dashboardApp().run()
+    def callback_5(self, event): 
+        self.input_text += '5'
+        self.pin_index += 1
+        if self.pin_index == self.pin_length:
+            if self.input_text == self.pin:
+                App.get_running_app().stop()
+                dashboardApp().run()
+    def callback_6(self, event): 
+        self.input_text += '6'
+        self.pin_index += 1
+        if self.pin_index == self.pin_length:
+            if self.input_text == self.pin:
+                App.get_running_app().stop()
+                dashboardApp().run()
+    def callback_7(self, event): 
+        self.input_text += '7'
+        self.pin_index += 1
+        if self.pin_index == self.pin_length:
+            if self.input_text == self.pin:
+                App.get_running_app().stop()
+                dashboardApp().run()
+    def callback_8(self, event): 
+        self.input_text += '8'
+        self.pin_index += 1
+        if self.pin_index == self.pin_length:
+            if self.input_text == self.pin:
+                App.get_running_app().stop()
+                dashboardApp().run()
+    def callback_9(self, event): 
+        self.input_text += '9'
+        self.pin_index += 1
+        if self.pin_index == self.pin_length:
+            if self.input_text == self.pin:
+                App.get_running_app().stop()
+                dashboardApp().run()
+    def callback_clear(self, event): 
+        self.input_text = ""
+        self.pin_index = 0
+      
+    
+
+     
+
+root = pinApp() 
+  
+# run function runs the whole program 
+# i.e run() method which calls the target 
+# function passed to the constructor. 
+root.run() 
+
+
+
+
+
+
+class FloatLayout(FloatLayout):    #root widget, main logic class
+
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
 
 
 
